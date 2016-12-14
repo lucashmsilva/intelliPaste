084f827d9693642577a2327d634d0992306d7a48
lucas <lucashenrique580@gmail.com>
2016-12-14 14:31:48 -0200

    initial commit

---
+++ intellipaste.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import json
+import requests
+import pyperclip
+
+API_KEY = os.environ.get('API_KEY')
+# API_KEY = "key=AIzaSyB5TfsLygRIzw8AvlrjlWc3wN2p8seT-0o"
+
+
+def google_url_shorten(url):
+    req_url = "https://www.googleapis.com/urlshortener/v1/url?" + API_KEY
+    payload = {'longUrl': url}
+    headers = {'content-type': 'application/json'}
+    r = requests.post(req_url, data=json.dumps(payload), headers=headers)
+    resp = json.loads(r.text)
+    return resp["id"]
+
+
+recent_value = ""
+while True:
+    tmp_value = pyperclip.paste()
+    if (tmp_value != recent_value and not tmp_value.startswith("https://goo.gl") and not tmp_value.startswith("https://git")):
+        recent_value = tmp_value
+        url = str(recent_value)
+        if url.startswith("http://") or url.startswith("https://") or url.startswith("www."):
+            pyperclip.copy(google_url_shorten(url))
+    time.sleep(0.5)
